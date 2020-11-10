8ac96aa33a23e4919ac1058c8c397271b03b85d6
nyaiQ <e1b18022@oit.ac.jp>
2020-11-10 15:44:01 +0900

    初期コミット

---
+++ src/main/java/oit/is/nksk/bavarder/BavarderApplication.java
@@ -0,0 +1,13 @@
+package oit.is.nksk.bavarder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BavarderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BavarderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/nksk/bavarder/BavarderApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.nksk.bavarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BavarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
