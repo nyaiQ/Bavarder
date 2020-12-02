6d78890f53e6f430e2d8820959b4053f2e763d72
nyaiQ <e1b18022@oit.ac.jp>
2020-12-02 15:39:49 +0900

    スプリングブートのアップデート

---
+++ src/test/java/oit/is/nksk/babarder/BabarderApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.nksk.babarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
