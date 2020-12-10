a10d0324842e238ef0416b94468b4e3287915e8d
suzukitatsuya18043 <e1b18043@oit.ac.jp>
2020-12-10 18:04:21 +0900

    いいね機能の実装および時間表示の修正

--- src/main/java/oit/is/nksk/bavarder/model/ChatMapper.java
+++ src/main/java/oit/is/nksk/bavarder/model/ChatMapper.java
@@ -14,18 +14,15 @@ public interface ChatMapper {
   @Select("SELECT * FROM chat")
   ArrayList<Chat> selectAllChat();
 
-  @Select("SELECT IINE FROM chat WHERE ID=#{id}")
-  Chat UniqueIine(int id);
-
   /**
    * @param chat
    */
-  @Insert("INSERT INTO chat (user,message,time) VALUES (#{user},#{message},#{time},#{iine});")
+  @Insert("INSERT INTO chat (user,message,time,iine) VALUES (#{user},#{message},#{time},#{iine});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertChat(Chat chat);
 
 
-  @Update("UPDATE CHAT SET IINE=#{iine}, WHERE ID = #{id}")
-  void updateiinecount(int iine , int id);
+  @Update("UPDATE CHAT SET iine = iine+1 WHERE ID = #{id}")
+  void updateiinecount(int id);
 
 }
